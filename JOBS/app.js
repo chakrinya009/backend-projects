61ddba28143a4c43a494ceb58f19ab7db8b90cd3
chakrinya <chakrinyabalabadra@gmail.com>
2023-06-27 10:42:16 +0530

    error handle

--- JOBS/app.js
+++ JOBS/app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -18,6 +25,15 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.use(
+  rateLimit({
+    windows: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 requests per windows
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
--- JOBS/middleware/error-handler.js
+++ JOBS/middleware/error-handler.js
@@ -1,16 +1,20 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-
-let customError={
-  //set default
-  
-}
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    //set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Somethingg went wrong try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  const errorHandlerMiddleware = (err, req, res, next) => {
+    if (err instanceof CustomAPIError) {
+      return res.status(err.statusCode).json({ msg: err.message });
+    }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    return req.status(customError.statusCode).json({ msg: customError, msg });
+  };
+};
+
+module.exports = errorHandlerMiddleware;
